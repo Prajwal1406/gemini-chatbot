6b35d16994e6eca0272c6027673d91d6d33971fe
Prajwal <praj3009wal@gmail.com>
2024-01-28 16:54:39 +0530

    dark theme

---
+++ gemchat.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai 
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response = chat.send_message(question, stream=True)
+    return response
+
+st.set_page_config(page_title="Q&A gemini")
+st.header("Prajwals Pa")
+
+if 'Chat_history' not in st.session_state:
+    st.session_state['Chat_history'] = []
+
+input_text = st.text_area("Input:",key=101)
+submit = st.button("Get Your Answer")
+
+if submit and input_text:
+    response = get_gemini_response(input_text)
+    st.session_state["Chat_history"].append(("You", input_text))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.code(chunk.text)
+        st.session_state["Chat_history"].append(("Bot", chunk.text))
+
+st.subheader("Chat History")
+for role, text in st.session_state["Chat_history"]:
+    st.code(f"{role}: {text}")
+
+
