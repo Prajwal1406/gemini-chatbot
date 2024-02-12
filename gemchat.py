6f0514a6c5c32eaca9394d5e1cde8eab7f201a66
Prajwal J P <119681594+Prajwal1406@users.noreply.github.com>
2024-02-13 00:41:55 +0530

    Update gemchat.py

--- gemchat.py
+++ gemchat.py
@@ -64,8 +64,13 @@ def maha():
         def get_conversational_chain():
         
             prompt_template = """
-            Answer the question as detailed as possible make sure to provide correct answer 
-            """
+            Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
         
             model = ChatGoogleGenerativeAI(model="gemini-pro",
                                      temperature=0.3)
